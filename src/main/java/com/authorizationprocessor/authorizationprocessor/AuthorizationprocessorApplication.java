ddc242d9f8a0e2ede10503e5f1ed24d999320d61
unknown <gjosemanuel469@gmail.com>
2024-02-15 13:17:14 -0500

    first commit

---
+++ src/main/java/com/authorizationprocessor/authorizationprocessor/AuthorizationprocessorApplication.java
@@ -0,0 +1,13 @@
+package com.authorizationprocessor.authorizationprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationprocessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationprocessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/authorizationprocessor/authorizationprocessor/AuthorizationprocessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authorizationprocessor.authorizationprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationprocessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
